674a6e371414bc09187655dba85648d7d2b23657
SangjaeJin <38873936+SangjaeJin@users.noreply.github.com>
2018-11-28 15:29:26 +0900

    Update Shoppingbag.java

--- phase3/Shoppingbag.java
+++ phase3/Shoppingbag.java
@@ -1,5 +1,4 @@
 
-
 public class Shoppingbag {
 	public static String getSelectQuery(String id) {
 		return "SELECT AMOUNT FROM SHOPPINGBAG WHERE C_id="+"\"" +id+"\"";
@@ -12,5 +11,10 @@ public class Shoppingbag {
 	
 	public static String getCreateBagQuery(String id) {
 		return "INSERT INTO SHOPPINGBAG VALUES(0,"  +"\""+id+"\""+")";
+	}
+	public static String getUpdateAmountQuery(String id,int amt)
+	{
+		amt-=1;
+		return "UPDATE SHOPPINGBAG SET AMOUNT="+amt+" WHERE C_id="+"\""+id+"\"";
 	}
 }
